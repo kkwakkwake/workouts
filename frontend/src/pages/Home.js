3e49779a59d9cda6d712c16e6f6637f9078b93e0
kkwakkwake <glowupup@naver.com>
2022-09-03 21:54:07 +0900

    feat: create,delete workout

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,28 +1,32 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
+
+import WorkoutDetails from '../components/WorkoutDetails';
+import WorkoutForm from '../components/WorkoutForm';
 
 const Home = () => {
-  const localserver = 'http://localhost:400'
-  const [workouts, setWorkouts] = useState(null)
+  const { workouts, dispatch } = useWorkoutsContext();
 
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch(`${localserver}/api/workouts`);
+      const response = await fetch('/api/workouts');
       const data = await response.json();
 
       if (response.ok) {
-        setWorkouts(data)
+        dispatch({ type: 'SET_WORKOUTS', payload: data })
       }
     }
 
     fetchWorkouts()
-  }, [])
+  }, [dispatch])
   return (
     <div className='home'>
       <div className='workouts'>
         {workouts && workouts.map((workout) => (
-          <p key={workout._id}>{workout.title}</p>
+          <WorkoutDetails key={workout._id} workout={workout} />
         ))}
       </div>
+      <WorkoutForm />
     </div>
   )
 }
